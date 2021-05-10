bd79e612f6fdf6cf21912accd27b61ebc26c7854
Sipos Aron David <sipisipos@gmail.com>
2021-05-10 16:11:19 +0200

    Refactored PlayCommand for maintainability

--- source/commands/voice/music/PlayCommand.js
+++ source/commands/voice/music/PlayCommand.js
@@ -3,6 +3,7 @@ const CommandTypeEnum = require("../../CommandTypeEnum");
 const ytSearch = require("yt-search");
 const validURL = require("../../../components/music/UrlChecker.js");
 const getYouTubePlaylistId = require("../../../components/music/UrlPlaylist.js");
+const { VoiceChannel, Message } = require("discord.js");
 
 class PlayCommand extends Command {
     name = "play";
@@ -22,44 +23,69 @@ class PlayCommand extends Command {
                 message
             );
             if (validURL(args[0])) {
-                let id = getYouTubePlaylistId(args[0]);
-                if (id != null)
-                    elia.musicComponent.musicQueue.playYouTubePlaylist(
-                        message,
-                        voiceChannel,
-                        id
-                    );
-                else
-                    elia.musicComponent.musicQueue.playMusic(
-                        message,
-                        voiceChannel,
-                        args[0]
-                    );
+                this.playFromYouTube(voiceChannel, message);
             } else {
-                const videoFinder = async (query) => {
-                    const videoResult = await ytSearch(query);
-                    return videoResult.videos.length > 1
-                        ? videoResult.videos[0]
-                        : null;
-                };
-
-                const video = await videoFinder(args.join(" "));
-
-                if (video) {
-                    elia.musicComponent.musicQueue.playMusic(
-                        message,
-                        voiceChannel,
-                        video.url,
-                        video.title
-                    );
-                } else {
-                    elia.messageComponent.reply(
-                        message,
-                        "No video results found."
-                    );
-                }
+                this.searchAndPlayFromYouTube(
+                    voiceChannel,
+                    message,
+                    args.join(" ")
+                );
             }
         }
+    }
+
+    /**
+     * Play's a video or playlist from YouTube
+     *
+     * @param {VoiceChannel} voiceChannel the VoiceChannel to join
+     * @param {Message} message the message which requested the music
+     */
+    playFromYouTube(voiceChannel, message) {
+        const id = getYouTubePlaylistId(args[0]);
+        if (id != null)
+            elia.musicComponent.musicQueue.playYouTubePlaylist(
+                message,
+                voiceChannel,
+                id
+            );
+        else
+            elia.musicComponent.musicQueue.playMusic(
+                message,
+                voiceChannel,
+                args[0]
+            );
+    }
+
+    /**
+     * Searches a query in YouTube and then play's the first video result match, if result exits
+     *
+     * @param {VoiceChannel} voiceChannel the VoiceChannel to join
+     * @param {Message} message the message which requested the music
+     * @param {string} query the search terms in one string
+     */
+    searchAndPlayFromYouTube(voiceChannel, message, query) {
+        const video = this.videoFinder(query);
+        if (video) {
+            elia.musicComponent.musicQueue.playMusic(
+                message,
+                voiceChannel,
+                video.url,
+                video.title
+            );
+        } else {
+            elia.messageComponent.reply(message, "No video results found.");
+        }
+    }
+
+    /**
+     * Searches a srting on YouTube and get the fist result.
+     *
+     * @param {String} query the string to search on YouTube
+     * @returns {?String} the first result of the query or null if no results
+     */
+    async videoFinder(query) {
+        const videoResult = await ytSearch(query);
+        return videoResult.videos.length > 1 ? videoResult.videos[0] : null;
     }
 }
 
